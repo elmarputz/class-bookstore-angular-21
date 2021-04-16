aa79c5df6193ae9b7c252c491f722c948e0ba02a
Elmar Putz <ep@studiomitte.com>
2021-04-16 09:54:17 +0200

    form & validators

---
+++ src/app/book-form/book-form-error-messages.ts
@@ -0,0 +1,16 @@
+export class ErrorMessage {
+
+
+    constructor (
+        public forControl: string, 
+        public forValidator: string, 
+        public text : string
+
+    ) {}
+}
+
+export const BookFormErrorMessages = [
+
+    new ErrorMessage('title', 'required', 'Ein Buchtitel muss angegeben werden')
+
+]
--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from "@angular/forms";
 import { BookFactory } from '../shared/book-factory';
 import { BookStoreService } from "../shared/book-store.service";
+import { BookFormErrorMessages } from "./book-form-error-messages";
+import { ThrowStmt } from "@angular/compiler";
 
 
 @Component({
@@ -17,6 +19,7 @@ export class BookFormComponent implements OnInit {
   book = BookFactory.empty();
   isUpdatingBook = false; 
   images: FormArray;
+  errors: { [key: string]: string } = {};
 
 
   constructor(
@@ -29,10 +32,62 @@ export class BookFormComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.bookForm = this.fb.group({
 
-      
+    const isbn = this.route.snapshot.params["isbn"];
+    if (isbn) {
+      this.isUpdatingBook = true;
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+        this.initBook();
+      });
+    }
+
+    this.initBook();
+  }
+
+  initBook() {
+    this.bookForm = this.fb.group({
+      id: this.book.id,  
+      title: [this.book.title, Validators.required], 
+      subtitle: this.book.subtitle, 
+      isbn: [
+        this.book.isbn, [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(13)
+
+        ]
+      ], 
+      description: this.book.description,
+      published: this.book.published, 
+
     });
+
+    this.bookForm.statusChanges.subscribe(() => 
+      this.updateErrorMessages()
+    );
+  }
+
+  updateErrorMessages() {
+    console.log("form invalid? " + this.bookForm.invalid);
+   
+    this.errors = {};
+
+    for (const message of BookFormErrorMessages) {
+      const control = this.bookForm.get(message.forControl); 
+
+      if (
+        control && 
+        control.dirty && 
+        control.invalid && 
+        control.errors[message.forValidator] && 
+        !this.errors[message.forControl] 
+      ) {
+        this.errors[message.forControl] = message.text;
+      }
+
+    }
+
   }
 
 }
